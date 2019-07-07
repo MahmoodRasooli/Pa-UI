c0c34c226e120301bc8274e11e141196e5900796
moghtaderi <moghtaderi@MOGHTADERI-PC>
2019-07-07 17:26:28 +0430

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class SignUp extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    username:'',
+    password:''
+    }
+  } 
+  
+   SignUp(){
+    console.log(this.state);
+   }
+  
+  render (){
+    return(
+    <form>
+    <div className="form-group">
+    <input type="text" name="username" className="form-control" onChange={event=>this.setState({username:event.target.value})} placeholder="username" ></input>
+    </div><br/>
+  
+    <div className="form-group"> <input type="password" name="password"  onChange={event=>this.setState({password:event.target.value})}  className="form-control" placeholder="password"></input></div> 
+    <button className="btn btn-success" onClick={()=>this.SignUp()}>signUp</button>
+  </form>
+    )
+  }
+  
+  }
 
 function App() {
   return (
@@ -8,8 +38,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          pashmam from app js.
         </p>
+<SignUp/>
         <a
           className="App-link"
           href="https://reactjs.org"
