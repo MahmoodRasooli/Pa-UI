98bd2fc7369c09d7b1459ee0f2e5ee268f51b435
moghtaderi <moghtaderi@MOGHTADERI-PC>
2019-07-13 17:38:35 +0430

    core-Ui

--- src/App.js
+++ src/App.js
@@ -1,57 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+// import { renderRoutes } from 'react-router-config';
+import './App.scss';
 
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 
-class SignUp extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-    username:'',
-    password:''
-    }
-  } 
-  
-   SignUp(){
-    console.log(this.state);
-   }
-  
-  render (){
-    return(
-    <form>
-    <div className="form-group">
-    <input type="text" name="username" className="form-control" onChange={event=>this.setState({username:event.target.value})} placeholder="username" ></input>
-    </div><br/>
-  
-    <div className="form-group"> <input type="password" name="password"  onChange={event=>this.setState({password:event.target.value})}  className="form-control" placeholder="password"></input></div> 
-    <button className="btn btn-success" onClick={()=>this.SignUp()}>signUp</button>
-  </form>
-    )
+// Containers
+const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/Pages/Login'));
+const Register = React.lazy(() => import('./views/Pages/Register'));
+const Page404 = React.lazy(() => import('./views/Pages/Page404'));
+const Page500 = React.lazy(() => import('./views/Pages/Page500'));
+
+class App extends Component {
+
+  render() {
+    return (
+      <HashRouter>
+          <React.Suspense fallback={loading()}>
+            <Switch>
+              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
+            </Switch>
+          </React.Suspense>
+      </HashRouter>
+    );
   }
-  
-  }
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          pashmam from app js.
-        </p>
-<SignUp/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
